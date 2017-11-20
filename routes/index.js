98a8556d6cf43aceee7e398976cd8c7dc17a06bc
hollygoat <siyuanli0185@gmail.com>
2017-11-19 16:28:53 -0800

    set up to fetch index.html by /

--- routes/index.js
+++ routes/index.js
@@ -5,8 +5,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.send('hello world');
-    // res.sendFile(path.join(__dirname,'..','public','index.html'));
+    // res.send('hello world');
+    res.sendFile(path.join(__dirname,'..','public','index.html'));
 });
 
 module.exports = router;
